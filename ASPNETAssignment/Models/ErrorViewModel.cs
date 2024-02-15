8ac9d7bc47ee93bce2e9b6ff812e530d163d6434
Micaela Nilsson <micaela.nilsson85@gmail.com>
2024-02-15 16:36:53 +0100

    Cleaned up the code.

--- ASPNETAssignment/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASPNETAssignment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
