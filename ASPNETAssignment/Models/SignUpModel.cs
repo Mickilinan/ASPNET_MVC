d146630468faf6b42e041feea822ac8b754701d0
Hanna <hannagrehn@gmail.com>
2024-02-20 13:42:24 +0100

    viewmodel

---
+++ ASPNETAssignment/Models/SignUpModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETAssignment.Models;
+
+public class SignUpModel
+{
+    [Display(Name = "First name", Prompt = "Enter your first name", Order = 0)]
+    [Required(ErrorMessage = "Invalid first name")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Last name", Prompt = "Enter your last name", Order = 1)]
+    [Required(ErrorMessage = "Invalid last name")]
+    public string LastName { get; set; } = null!;
+
+    
+    [Display(Name = "Email", Prompt = "Enter your email address", Order = 2)]
+    [DataType(DataType.EmailAddress)]
+    [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid email adress")]
+    public string Email { get; set; } = null!;
+
+
+    [Display(Name = "Password", Prompt = "Enter your password", Order = 3)]
+    [DataType(DataType.Password)]
+    [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,}$", ErrorMessage = "Please enter a valid password")]
+    public string Password { get; set; } = null!;
+
+
+    [Display(Name = "Confirm password", Prompt = "Confirm your password", Order = 4)]
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password), ErrorMessage = "Password does not match")]
+    public string ConfirmPassword { get; set; } = null!;
+
+
+    [Display(Name = "I agree to the terms and conditions", Order = 5)]
+    [Required(ErrorMessage = "Invalid")]
+    public bool TermsAndConditions { get; set; } = false;
+
+}
---
+++ ASPNETAssignment/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ASPNETAssignment.ViewModels;
+
+public class SignUpViewModel
+{
+    public string PageTitle { get; set; } = "Sign up";
+    public SignUpViewModel Form { get; set; } /*= new SignUpViewModel();*/
+    public bool TermsAndConditions { get; set; } = false; 
+}
