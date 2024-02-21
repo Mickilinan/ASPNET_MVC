791e77c57e6de8b485a2b1d2ae37eae18cdf1516
sarahk189 <143951669+sarahk189@users.noreply.github.com>
2024-02-21 14:12:29 +0100

    Revert "Reapply "normalize all line endings""
    This reverts commit 51d126761b38b40b7d8553118d045190a4527462.

---
+++ ASPNETAssignment/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRouting(x => x.LowercaseUrls = true); 
+
+var app = builder.Build();
+
+
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
