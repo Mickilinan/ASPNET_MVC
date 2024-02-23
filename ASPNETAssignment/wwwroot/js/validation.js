283929403619910a5c8a0df1c8c18bfb8819dddf
Hanna <hannagrehn@gmail.com>
2024-02-23 17:42:49 +0100

    valivali

---
+++ ASPNETAssignment/wwwroot/js/validation.js
@@ -0,0 +1,21 @@
+﻿const lengthValidator = (value, minLength = 2) => {
+    if (value.Length >= minLength)
+        return true
+
+    return false
+}
+
+const compareValidator = (value, compareWithValue) => {
+    if (value === compareWithValue)
+        return true
+
+    return false
+
+}
+
+const checkedValidator = (element) => {
+    if (element.checked) 
+        return true 
+
+    return false
+}
