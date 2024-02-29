6e9e3c36c9803774713ab6385a9cb56cad5c9b6b
sarahk189 <143951669+sarahk189@users.noreply.github.com>
2024-02-29 12:48:02 +0100

    Account Details scss and forms scss

--- ASPNETAssignment/ViewModels/AccountDetailsViewModel.cs
+++ ASPNETAssignment/ViewModels/AccountDetailsViewModel.cs
@@ -6,7 +6,13 @@ namespace ASPNETAssignment.ViewModels
     {
         public string Title { get; set; } = "Account Details";
 
-        public AccountDetailsBasicInfoModel BasicInfo { get; set; } = new AccountDetailsBasicInfoModel();
+        public AccountDetailsBasicInfoModel BasicInfo { get; set; } = new AccountDetailsBasicInfoModel()
+        {
+            ProfileImage = "~/Images/contactDetailsImages/profile_image.svg",
+            FirstName = "Sarah",
+            LastName = "Kriborg",
+            Email = "sarah.kriborg@domain.com"
+        };
 
         public AccountDetailsAddressInfoModel AddressInfo { get; set; } = new AccountDetailsAddressInfoModel();
     }
