c66fef585b4e283030bbf94dafccdf99466cad13
Micaela Nilsson <micaela.nilsson85@gmail.com>
2024-03-02 12:29:01 +0100

    Database setup.

--- ASPNETAssignment/Program.cs
+++ ASPNETAssignment/Program.cs
@@ -1,7 +1,13 @@
+using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRouting(x => x.LowercaseUrls = true); 
+
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
